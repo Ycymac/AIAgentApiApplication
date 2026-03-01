158945d43e72a863df1b2a5594b999157cfdaff8
Marc Yin <3253984909@qq.com>
2026-03-01 21:08:12 +0800

    feature:实现通过用户背景 调用api生成15个切合专业、学习方向的面试问题

---
+++ agent/src/main/java/com/ycy/agent/common/pojo/InterviewQuestion.java
@@ -0,0 +1,23 @@
+package com.ycy.agent.common.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InterviewQuestion {
+
+    /**
+     * 问题对应的等级
+     */
+    private int level;
+    /**
+     * 问题对应的问题描述
+     */
+    private String questionDescription;
+
+}
