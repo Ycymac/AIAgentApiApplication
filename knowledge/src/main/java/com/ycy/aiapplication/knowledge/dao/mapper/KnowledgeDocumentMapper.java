5b7cdd1a71bbf5e487b46584434cf03f7bb22912
Marc Yin <3253984909@qq.com>
2026-04-02 15:36:06 +0800

    knowledge-fix:补全知识库文件持久化层

---
+++ knowledge/src/main/java/com/ycy/aiapplication/knowledge/dao/mapper/KnowledgeDocumentMapper.java
@@ -0,0 +1,9 @@
+package com.ycy.aiapplication.knowledge.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ycy.aiapplication.knowledge.dao.entity.KnowledgeDocumentDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface KnowledgeDocumentMapper extends BaseMapper<KnowledgeDocumentDO> {
+}
